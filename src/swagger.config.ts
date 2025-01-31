b41070c24c565782c12e30a6ae8196982f1da92c
songminkyu <rabbircarrot@naver.com>
2025-01-31 13:39:53 +0900

    first commit

---
+++ src/swagger.config.ts
@@ -0,0 +1,12 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('Test API Docs') // API 명
+    .setDescription('NestJS Swagger Test') // API 설명
+    .setVersion('1.0.0') // 버전
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document); // API 문서 주소
+}
